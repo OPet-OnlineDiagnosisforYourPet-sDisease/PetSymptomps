b5ce472b81cf0ae39cee92c5c69015342f8c7502
rozisaputra24 <82568840+rozisaputra24@users.noreply.github.com>
2023-05-23 14:47:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const routes = require('./routes');
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 
