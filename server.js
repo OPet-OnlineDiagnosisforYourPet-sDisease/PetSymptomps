eab3a8a9c9e45aa41464b11ecb6af97d6c48f350
rozisaputra24 <82568840+rozisaputra24@users.noreply.github.com>
2023-05-24 22:17:50 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const routes = require('./routes');
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/', routes);
-
 
 app.listen(port, () => {
     console.log(`Server berjalan pada http://localhost:${port}`);
