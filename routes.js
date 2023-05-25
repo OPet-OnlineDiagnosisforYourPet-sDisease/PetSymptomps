26c3481748997c5ca5be336280eb73897df06037
rozisaputra24 <82568840+rozisaputra24@users.noreply.github.com>
2023-05-25 12:02:56 +0700

    Update routes.js

--- routes.js
+++ routes.js
@@ -2,25 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { postGejala, getGejala } = require('./handler');
 
-const routes = [
-  {
-    method: 'POST',
-    path: '/gejala',
-    handler: postGejala,
-  },
-  {
-    method: 'GET',
-    path: '/gejala',
-    handler: getGejala,
-  },
-];
-
-routes.forEach((route) => {
-  if (route.method === 'POST') {
-    router.post(route.path, route.handler);
-  } else if (route.method === 'GET') {
-    router.get(route.path, route.handler);
-  }
-});
+router.post('/gejala', postGejala);
+router.get('/gejala', getGejala);
 
 module.exports = router;
